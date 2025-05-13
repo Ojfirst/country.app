460b17e76841d4c1d1f33d36efd21c255f04fdec
Simon <omojide.o.j@gmail.com>
2025-05-13 05:35:57 +0100

    Feat: Add html elements for country info and JavaScript API data to display info

--- main.js
+++ main.js
@@ -19,8 +19,28 @@ const init = () => {
       const rawData = await fetchAPI(city);
       console.log(rawData);
       const processedData = {
+        officailName: rawData[0].name.official,
+        flag: rawData[0].flags.png,
+        flagAlt: rawData[0].flags.alt,
+        countryName: rawData[0].name.common,        
         countryCapital: rawData[0].capital,
-      };
+        continents: rawData[0].continents,
+        population: rawData[0].population,
+        currencyName: Object.keys(rawData[0].currencies)[0],
+        currencySymbol: Object.values(rawData[0].currencies)[0].symbol,
+        area: rawData[0].area,
+        language: Object.values(rawData[0].languages).join(', '),
+
+        // Region
+        map: rawData[0].maps.openStreetMaps,
+        region: rawData[0].region,
+        subregion: rawData[0].subregion,
+        lat: rawData[0].latlng[0],
+        lng: rawData[0].latlng[1],
+        // Timezone
+        timeZone: rawData[0].timezones,
+
+      }
       console.log(processedData);
 
       countryUI.test(processedData);
--- modules/dom.js
+++ modules/dom.js
@@ -3,6 +3,31 @@
 // This is a test scrip
 export const countryUI = {
   test: (data) => {
-    document.querySelector('#test').textContent = data.countryCapital;
+    // This function is used to update the UI with the fetched data
+    
+    document.querySelector('#country-offical-name').textContent = data.officailName;   // Update the UI with the official name of the country
+    document.querySelector('#country-offical-flag').src = data.flag;   // Update the UI with the flag of the country
+    document.querySelector('#country-offical-flag').alt = data.flagAlt;   // Update the UI with the alt text of the flag
+    document.querySelector('#country-name').textContent = data.countryName;
+    document.querySelector('#country-capital').textContent = data.countryCapital;
+    document.querySelector('#continents').textContent = data.continents;
+    document.querySelector('#population').textContent = data.population;
+
+    document.querySelector('#area').textContent = data.area;
+    document.querySelector('#language').textContent = data.language;
+    
+    document.querySelector('#currency-name').textContent = data.currencyName;
+    document.querySelector('#currency-symbol').textContent = data.currencySymbol;
+    
+
+    // Region
+    document.querySelector('#maps').href = data.map;
+
+    document.querySelector('#region').textContent = data.region;
+    document.querySelector('#sub-region').textContent = data.subregion;
+    document.querySelector('#lat').textContent = data.lat;
+    document.querySelector('#lng').textContent = data.lng;
+    // Timezone
+    document.querySelector('#time-zone').textContent = data.timeZone;
   }
 }
